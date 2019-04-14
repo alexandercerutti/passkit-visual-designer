336f43386f5c5c58dd711fb6b7e93be7e897b4dd
Alexander Cerutti <cerutti.alexander@gmail.com>
2019-04-14 22:34:05 +0200

    Project setup

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class App extends React.Component<{}, {}> {
+	render() {
+		return (
+			<h1>
+				Hello World
+			</h1>
+		)
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	target: "web",
+	entry: "./src/public/index.tsx",
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			loader: "babel-loader",
+			exclude: /(node_modules)/,
+			options: {
+				presets: ["@babel/preset-react"]
+			}
+		}, {
+			test: /\.tsx?$/,
+			loader: "awesome-typescript-loader"
+		}, {
+			test: /\.less$/,
+			loader: "less-loader"
+		}]
+	},
+	devtool: "source-map",
+	resolve: {
+		extensions: [".js", ".jsx", ".ts", ".tsx"]
+	},
+	devServer: {
+		port: 3000,
+		host: "0.0.0.0",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Passkit Visual Designer",
+			template: "./src/public/index.html",
+			filename: "./index.html"
+		})
+	]
+};
