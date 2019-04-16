38bbfcd74d0b0d3bc433d9e1caece9ab026b09d9
Alexander Cerutti <cerutti.alexander@gmail.com>
2019-04-16 23:34:21 +0200

    Added more styles to create the first view

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,69 @@
 import * as React from "react";
+import Pass from "./pass";
 
-export default class App extends React.Component<{}, {}> {
+interface AppState {
+	phase: number;
+}
+
+export default class App extends React.Component<{}, AppState> {
+	constructor(props: {}) {
+		super(props);
+
+		this.state = {
+			phase: 0
+		};
+	}
+
 	render() {
+		// if (this.state.phase === 0) {
+		// 	return
+		// }
+
 		return (
-			<h1>
-				Hello World
-			</h1>
-		)
+			<div id="app">
+				<PassArea />
+				<Configurator />
+			</div>
+		);
+	}
+}
+
+interface ConfiguratorProps {
+	visible?: boolean
+}
+
+class Configurator extends React.Component<ConfiguratorProps, {}> {
+	render(): JSX.Element {
+		return (
+			<div id="configurator" style={{ width: this.props.visible ? "300px" : "0"}}>
+				<h1>Configurator</h1>
+			</div>
+		);
+	}
+}
+
+class PassArea extends React.Component {
+	passTypes = ["boardingPass", "eventTicket", "coupon", "generic", "storeCard"];
+
+	constructor(props: {}) {
+		super(props);
+
+		this.onPassClick = this.onPassClick.bind(this);
+	}
+
+	onPassClick() {
+		console.log("PASS CLICKED")
+	}
+
+	render() {
+		const passes = this.passTypes.map((pass) => (
+			<Pass key={pass} type={pass} onClick={this.onPassClick} />
+		));
+
+		return (
+			<div className="selector">
+				{passes}
+			</div>
+		);
 	}
 }
--- src/public/index.tsx
+++ src/public/index.tsx
@@ -1,5 +1,6 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import "./styles.less";
 import App from "../app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
