00a1141f17b98cf70a67d225f84b552b6edd25ef
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-01-12 23:25:45 +0100

    Added initial base pass component with styles

---
+++ src/passes/base.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import "base.less";
+
+interface BasePassProps {
+
+}
+
+export default class Pass extends React.Component<BasePassProps> {
+	constructor(props: any) {
+		super(props);
+	}
+
+	render(): JSX.Element {
+		return (
+			<div></div>
+		)
+	}
+}
