94328dce4ee40c404e29a3e0c10eff9502efa18f
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-01-22 00:15:05 +0100

    Added base passes rendering

--- src/PassSelector/index.tsx
+++ src/PassSelector/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import "./style.less";
 import App from "../app";
+import { PassKind } from "../model";
+import Pass from "../passes/base";
 
 interface SelectorProps {
 	onPassKindSelection: App["onPassKindSelection"];
 }
 
-export class PassSelector extends React.Component<SelectorProps> {
+export class PassSelector extends React.PureComponent<SelectorProps> {
 	private config = {
 		introText: "Select your pass model"
 	};
@@ -16,9 +18,14 @@ export class PassSelector extends React.Component<SelectorProps> {
 	}
 
 	render() {
+		const passes = Object.keys(PassKind).map((pass) => (
+			<Pass key={PassKind[pass]} onClick={(e) => this.props.onPassKindSelection(PassKind[pass])} />
+		));
+
 		return (
 			<div id="selector">
 				<h2>{this.config.introText}</h2>
+				{passes}
 			</div>
 		);
 	}
