69c8973447794f65bc6164d5860d267bd3a28fd7
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-01-31 00:08:10 +0100

    Updated pass base with first kind-driven rendering

--- src/passes/base.tsx
+++ src/passes/base.tsx
@@ -1,20 +1,39 @@
 import * as React from "react";
 import "./base.less";
+import { PassKind } from "../model";
+import BoardingPass from "./BoardingPass";
 
-interface BasePassProps {
-	onClick(evt: React.MouseEvent): void;
+export interface PassProps {
+	kind: PassKind;
+	subKind?: any;
+	onClick?(evt: React.MouseEvent): void;
 }
 
-export default class Pass extends React.Component<BasePassProps> {
+export default class Pass extends React.Component<PassProps> {
 	constructor(props: any) {
 		super(props);
 	}
 
+	deriveComponentFromKind(kind: PassKind): React.ComponentType<PassProps> {
+		switch (kind) {
+			case PassKind.BOARDING_PASS:
+				return BoardingPass
+
+			default:
+				return BoardingPass;
+		}
+	}
+
 	render(): JSX.Element {
+		const PassComponent = this.deriveComponentFromKind(this.props.kind);
+		const PassProps = ({ kind, subKind }: PassProps) => ({ kind, subKind });
+
+		console.log(this.props, PassComponent);
+
 		return (
-			<div className="base-pass" onClick={this.props.onClick}>
-				<div className="base-content">
-					Test
+			<div className="pass select" onClick={this.props.onClick}>
+				<div className="content">
+					<PassComponent {...PassProps(this.props)} />
 				</div>
 			</div>
 		);
