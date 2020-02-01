64d35e31b594c2c4d7007f9b117d1a93d47f3ee1
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-01 23:24:06 +0100

    First implementation of Field

---
+++ src/passes/Components/Field/index.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { FillableComponent } from "../FillableComponent";
+import withRegistration from "../withRegistration";
+import withFallback from "../EmptyField/withFallback";
+import { FieldKind } from "../../../model";
+import "./fields.less";
+import { PKTextAlignment, PKDataDetectorType, PKDateStyle } from "../../constants";
+
+export interface FieldSetProps extends FillableComponent {
+	className?: string;
+	labelColor?: string;
+	textColor?: string;
+	key: string;
+	label?: string;
+	value: string;
+
+	// to be implemented
+	textAlignment: PKTextAlignment;
+	dataDetectorTypes: PKDataDetectorType;
+	changeMessage?: string; // check for @s
+	dateStyle?: PKDateStyle;
+	timeStyle?: PKDateStyle;
+}
+
+function PureFieldSet(props: FieldSetProps) {
+	return (
+		<div className={`${props.className || ""} field field-${props.key}`.trim()}>
+			{
+				props.label &&
+				<span style={{ color: props.labelColor || "#000" }}>
+					{props.label}
+				</span>
+			}
+			<span style={{ color: props.textColor || "#000" }}>
+				{props.value}
+			</span>
+		</div>
+	);
+}
+
+export default withRegistration(withFallback(PureFieldSet, ["value", "key"]), FieldKind.FIELDS);
