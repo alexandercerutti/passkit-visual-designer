941e7f595dcf3b96e05321e53b5572ceca248db3
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-01 19:08:01 +0100

    Changed interfaces type for FillableComponent

--- src/passes/Components/FillableComponent.ts
+++ src/passes/Components/FillableComponent.ts
@@ -1,9 +1,9 @@
-import { RegisterPassEditableField } from "./withRegistration";
+import { onRegister, onSelect } from "./withRegistration";
 
 export interface FillableComponent<T = any> {
 	id: string;
 	content?: T;
 	shouldRegister?: boolean;
-	onSelect?(id: string): void;
-	register?: RegisterPassEditableField;
+	onSelect?: onSelect;
+	register?: onRegister;
 }
