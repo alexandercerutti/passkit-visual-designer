6d004bc4065afa0411995d0c66194b13b3801d3a
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-02 18:10:27 +0100

    Added constants file with PK types

---
+++ src/passes/constants.ts
@@ -0,0 +1,7 @@
+export type PKTextAlignment = "PKTextAlignmentLeft" | "PKTextAlignmentCenter" | "PKTextAlignmentRight" | "PKTextAlignmentNatural";
+
+export type PKDataDetectorType = "PKDataDetectorTypePhoneNumber" | "PKDataDetectorTypeLink" | "PKDataDetectorTypeAddress" | "PKDataDetectorTypeCalendarEvent";
+
+export type PKDateStyle = "PKDateStyleNone" | "PKDateStyleShort" | "PKDateStyleMedium" | "PKDateStyleLong" | "PKDateStyleFull";
+
+export type PKTransitType = "PKTransitTypeAir" | "PKTransitTypeBoat" | "PKTransitTypeBus" | "PKTransitTypeGeneric" | "PKTransitTypeTrain";
