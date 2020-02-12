653ed4a5b2c79f35d79fc55047cf0db4394ac9c0
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-12 22:48:45 +0100

    Added basic structure for Boarding pass with raw fields and data examples

--- src/passes/BoardingPass/index.tsx
+++ src/passes/BoardingPass/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { PassProps } from "../base";
 import { PassHeader } from "../Components/Header";
-import { PKTransitType } from "../constants";
+import { PKTransitType, PKBarcodeFormat } from "../constants";
 import PrimaryFields from "./primaryFields";
+import FieldsRow from "../Components/FieldRow";
+import Barcode from "../Components/Barcodes";
 
 interface BoardingPassProps extends PassProps {
-	subKind: "PKTransitTypeAir" | "PKTransitTypeBoat" | "PKTransitTypeBus" | "PKTransitTypeGeneric" | "PKTransitTypeTrain";
+	subKind: PKTransitType;
 }
 
 export default class BoardingPass extends React.Component<BoardingPassProps> {
@@ -13,66 +15,98 @@ export default class BoardingPass extends React.Component<BoardingPassProps> {
 		super(props);
 	}
 
-	renderPKTransitTypeAir() {
-		return (
-			<>
-				<PassHeader />
-			</>
-		);
-	}
-
-	renderPKTransitTypeBoat() {
-		return (
-			<>
-				<PassHeader />
-			</>
-		);
-	}
-
-	renderPKTransitTypeBus() {
-		return (
-			<>
-				<PassHeader />
-			</>
-		);
-	}
-
-	renderPKTransitTypeGeneric() {
+	render(): JSX.Element {
 		return (
 			<>
 				<PassHeader
-					headerFieldsData={[{
-						label: "Data",
-						fieldKey: "departing_date",
-						value: "10/04/1996",
-					}, {
-						label: "Ora",
-						fieldKey: "departing_time",
-						value: "10:30",
-					}, {
-						label: "test",
-						fieldKey: "departing_time",
-						value: "10:30",
-					}]}
+					headerFieldsData={[]}
+					onSelect={(id: string) => console.log("Selected", id)}
+					register={(kind, id) => true}
+
+				/**
+				{
+					label: "Data",
+					fieldKey: "departing_date",
+					value: "10/04/1996",
+				}, {
+					label: "Ora",
+					fieldKey: "departing_time",
+					value: "10:30",
+				}, {
+					label: "test",
+					fieldKey: "departing_time",
+					value: "10:30",
+				}
+				 */
 				/>
-				{"BoardingPass"}
+				<PrimaryFields
+					subkind={this.props.subKind || PKTransitType.Generic}
+					primaryFieldsData={[]}
+					onSelect={(id: string) => console.log("Selected", id)}
+					register={(kind, id) => true}
+
+				/**
+				 * {
+					fieldKey: "starting_point",
+					value: "ARN",
+					label: "stockholm-arlanda"
+				}, {
+					fieldKey: "finish_point",
+					value: "CPH",
+					label: "copenhagen t2"
+				}
+				 */
+
+				/>
+				<FieldsRow
+					areaIdentifier="auxiliaryFields"
+					maximumElementsAmount={-1}
+					elements={[]}
+
+				/**
+				 * 	{
+						fieldKey: "passenger",
+						label: "passeggero",
+						value: "Alexander Patrick Cerutti"
+					},
+					{
+						fieldKey: "flight",
+						label: "n. volo",
+						value: "FR1328"
+					},
+					{
+						fieldKey: "seq",
+						label: "sequenza",
+						value: "8"
+					}
+				 */
+				/>
+				<FieldsRow
+					areaIdentifier="secondaryFields"
+					maximumElementsAmount={-1}
+					elements={[]}
+				/**
+				 * 	{
+						"fieldKey": "gateClose",
+						"label": "Il Gate Chiude",
+						"dateStyle": PKDateStyle.None,
+						"timeStyle": PKDateStyle.Short,
+						"value": "09:20"
+					},
+					{
+						"fieldKey": "queue",
+						"label": "Fila",
+						"value": "Priorità"
+					},
+					{
+						"fieldKey": "seat",
+						"label": "Posto*",
+						"value": "16C"
+					}
+				 */
+				/>
+				<Barcode format={PKBarcodeFormat.None} fallbackKind="square" />
 			</>
 		);
-	}
-
-	renderPKTransitTypeTrain() {
-		return (
-			<>
-				<PassHeader />
-			</>
-		);
-	}
-
-	render(): JSX.Element {
-		if (this.props.subKind) {
-			return this[`render${this.props.subKind}`]();
-		}
-
-		return this.renderPKTransitTypeGeneric();
 	}
 }
