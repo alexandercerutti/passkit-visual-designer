5dfe34b89b36538d8a60a65e2406f4ceea95d42d
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-13 22:27:11 +0100

    Added explanation comment

--- src/passes/BoardingPass/footerRow/index.tsx
+++ src/passes/BoardingPass/footerRow/index.tsx
@@ -12,6 +12,13 @@ interface FooterProps {
 export default (props: FooterProps) => {
 	return (
 		<div className="footer">
+			{
+				/*
+				 * This internal div, allows us to have an
+				 * image field with a width equal to the
+				 * barcode's.
+				 */
+			}
 			<div>
 				<ImageField {...props.footerImage} id="footer.image" />
 				<Barcode {...props.barcodeProps} fallbackKind="square" />
