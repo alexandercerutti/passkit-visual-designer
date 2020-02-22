8baff2c5083da51c251ec0d0fc2e822c14999053
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-22 20:03:13 +0100

    Setted barcodes to have 100% height

--- src/passes/Components/Barcodes/code128.tsx
+++ src/passes/Components/Barcodes/code128.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 export default () => (
-	<svg x="0" y="0" width="100%" height="30" viewBox="0.0000 0.0000 117 20" xmlns="http://www.w3.org/2000/svg">
+	<svg x="0" y="0" width="100%" height="100%" viewBox="0.0000 0.0000 117 20" xmlns="http://www.w3.org/2000/svg">
 		<rect x="0.0000" width="1.0583" height="20.1864" />
 		<rect x="1.5875" width="0.5292" height="20.1864" />
 		<rect x="3.1750" width="0.5292" height="20.1864" />
