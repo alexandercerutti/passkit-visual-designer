ec53ce53aec3c8a44c74b295422d29fe0b5e0e6c
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-02-29 18:42:47 +0100

    Added first version of Event Pass

--- src/passes/EventTicket/index.tsx
+++ src/passes/EventTicket/index.tsx
@@ -1,11 +1,118 @@
 import * as React from "react";
-import Pass, { PassProps } from "../base";
+import { PassProps } from "../base";
+import { PassHeader } from "../Components/Header";
+import ThumbnailPrimaryField from "../Components/PrimaryFields/Thumbnail";
+import FieldsRow from "../Components/FieldRow";
+import StripPrimaryFields from "../Components/PrimaryFields/Strip";
+import Footer from "../Components/Footer";
+import Barcodes from "../Components/Barcodes";
+import { PKBarcodeFormat } from "../constants";
 
-interface EventTicketProps extends PassProps { }
+interface EventTicketProps extends PassProps {
+	imagePosition: "strip" | "background";
+	src?: string;
+}
+
+type PrimaryFieldPropsKind = Parameters<(typeof StripPrimaryFields | typeof ThumbnailPrimaryField)>[0]
 
 export function EventTicket(props: EventTicketProps): JSX.Element {
+	let FieldsFragment: React.ReactElement<PrimaryFieldPropsKind>;
+
+	const SecondaryFieldRow = (
+		<FieldsRow
+			areaIdentifier="secondaryFields"
+			elements={[]}
+			maximumElementsAmount={4}
+		/>
+	);
+
+	/**
+		{
+			"fieldKey": "loc",
+			"label": "LOCATION",
+			"value": "Moscone West"
+		}, {
+			"fieldKey": "loc",
+			"label": "LOCATION",
+			"value": "Moscone West"
+		}, {
+			"fieldKey": "loc",
+			"label": "LOCATION",
+			"value": "Moscone West"
+		}, {
+			"fieldKey": "loc",
+			"label": "LOCATION",
+			"value": "Moscone West"
+		}, {
+			"fieldKey": "loc",
+			"label": "LOCATION",
+			"value": "Moscone West"
+		}, {
+			"fieldKey": "loc",
+			"label": "LOCATION",
+			"value": "Moscone West"
+		}
+	 */
+
+	// const imagePosition = "thumbnail";
+
+	if (props.imagePosition === "strip") {
+		FieldsFragment = (
+			<>
+				<StripPrimaryFields
+					stripSrc={props.src}
+					primaryFieldsData={[]}
+				/>
+				{SecondaryFieldRow}
+			</>
+		);
+
+		/**
+		{
+			"fieldKey": "event",
+			"label": "EVENT",
+			"value": "The Beat Goes On"
+		}
+		 */
+
+	} else {
+		FieldsFragment = (
+			<ThumbnailPrimaryField
+				thumbnailSrc={props.src}
+				primaryFieldsData={[]}
+			>
+				{SecondaryFieldRow}
+			</ThumbnailPrimaryField>
+		);
+
+		/**
+		{
+			"fieldKey": "event",
+			"label": "EVENT",
+			"value": "The Beat Goes On"
+		}
+		 */
+	}
+
 	return (
 		<>
+			<PassHeader
+				headerFieldsData={[]}
+				onClick={(id: string) => console.log("Selected", id)}
+				register={(kind, id) => true}
+			/>
+			{FieldsFragment}
+			<FieldsRow
+				areaIdentifier="auxiliaryFields"
+				maximumElementsAmount={4}
+				elements={[]}
+			/>
+			<Footer>
+				<Barcodes
+					format={PKBarcodeFormat.None}
+					fallbackKind="square"
+				/>
+			</Footer>
 		</>
 	);
 }
