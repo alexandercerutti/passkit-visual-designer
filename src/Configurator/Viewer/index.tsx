30319287e148cab8cb0f194a22b95c5875d59075
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-04-06 23:23:49 +0200

    First folder structuing for Configurator

---
+++ src/Configurator/Viewer/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "style.less";
+
+export default function Viewer() {
+	return (
+		<div></div>
+	);
+}
---
+++ src/Configurator/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import "style.less";
+
+interface ConfiguratorProps { }
+
+export default class Configurator extends React.Component<ConfiguratorProps> {
+	constructor(props: ConfiguratorProps) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
