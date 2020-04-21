b5bf46f75a038082a3164d47ad41d89542114ac6
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-04-22 00:30:54 +0200

    Setted panel group to act in different ways if it is selected or not

--- src/Configurator/OptionsMenu/PanelGroup/index.tsx
+++ src/Configurator/OptionsMenu/PanelGroup/index.tsx
@@ -1,19 +1,25 @@
 import * as React from "react";
 import "./style.less";
 import ArrowDownIcon from "./arrow";
+import { concatClassNames } from "../../../passes/utils";
 
 interface GroupProps {
 	name: string;
+	isActive?: boolean;
+	setActive?: (name: string) => void;
 }
 
 export default function PanelGroup(props: React.PropsWithChildren<GroupProps>) {
+	const className = concatClassNames("menu-group", props.isActive && "open");
+
 	return (
-		<div className="menu-group" data-name={props.name}>
-			<div className="intro">
+		<div className={className} data-name={props.name}>
+			<div className="intro" onClick={() => props.setActive(props.name)}>
 				<h3>{props.name}</h3>
-				<ArrowDownIcon style={{ width: "20px", fill: "#333" }} />
+				<ArrowDownIcon className="arrow-icon" />
 			</div>
 			<div className="panels-list">
+				<span>Here are your unstyled panels!</span>
 				{props.children}
 			</div>
 		</div>
