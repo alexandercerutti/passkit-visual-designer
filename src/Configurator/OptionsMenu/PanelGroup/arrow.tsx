ba6c9b7c70a038ae6b19eca61b57038025c9611c
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-04-21 22:52:20 +0200

    Improved PanelGroup content with arrow icon

---
+++ src/Configurator/OptionsMenu/PanelGroup/arrow.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function ArrowDownIcon(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg viewBox="0 0 668 375" {...props}>
+			<path d="M363 363L656 71c17.1-18.17 15.92-46.62-3.53-62.15-16.68-13.33-40.89-11.43-56 3.67L334 246.5 71 12C54-4 28-4 12 12s-16 42 0 59l292 292c17 16 43 16 59 0z" />
+		</svg>
+	);
+}
--- src/Configurator/OptionsMenu/PanelGroup/index.tsx
+++ src/Configurator/OptionsMenu/PanelGroup/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./style.less";
+import ArrowDownIcon from "./arrow";
 
 interface GroupProps {
 	name: string;
@@ -7,8 +8,14 @@ interface GroupProps {
 
 export default function PanelGroup(props: React.PropsWithChildren<GroupProps>) {
 	return (
-		<div className="menu-group">
-			{props.children}
+		<div className="menu-group" data-name={props.name}>
+			<div className="intro">
+				<h3>{props.name}</h3>
+				<ArrowDownIcon style={{ width: "20px", fill: "#333" }} />
+			</div>
+			<div className="panels-list">
+				{props.children}
+			</div>
 		</div>
 	);
 }
