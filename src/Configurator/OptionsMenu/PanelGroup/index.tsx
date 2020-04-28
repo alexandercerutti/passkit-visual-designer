a1538a47612c93bde5cb1eb65f1ae868a9776f82
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-04-28 23:01:40 +0200

    Changed arrow icon to category-specific icon

--- src/Configurator/OptionsMenu/PanelGroup/index.tsx
+++ src/Configurator/OptionsMenu/PanelGroup/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import "./style.less";
-import ArrowDownIcon from "./arrow";
 import { concatClassNames } from "../../../passes/utils";
 
 interface GroupProps {
 	name: string;
+	icon: JSX.Element;
 	isActive?: boolean;
 	setActive?: (name: string) => void;
 }
@@ -12,11 +12,13 @@ interface GroupProps {
 export default function PanelGroup(props: React.PropsWithChildren<GroupProps>) {
 	const className = concatClassNames("menu-group", props.isActive && "open");
 
+	const icon = React.cloneElement(props.icon, { className: "icon" });
+
 	return (
 		<div className={className} data-name={props.name}>
 			<div className="intro" onClick={() => props.setActive(props.name)}>
 				<h3>{props.name}</h3>
-				<ArrowDownIcon className="arrow-icon" />
+				{icon}
 			</div>
 			<div className="panels-list">
 				<span>Here are your unstyled panels!</span>
--- src/Configurator/OptionsMenu/PanelNavigator/index.tsx
+++ src/Configurator/OptionsMenu/PanelNavigator/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import "./style.less";
-import { RegisteredFieldsMap } from "..";
+import { RegisteredFieldsMap, DataGroup } from "..";
 import PanelGroup from "../PanelGroup";
-import Panel from "../Panel";
+import Panel, { FieldDetails } from "../Panel";
+import { TagIcon, ColorIcon, ImagesIcon, DataIcon } from "../PanelGroup/icons";
 
 interface NavigatorState {
 	pagePanelsHierarchy: React.ReactNode[];
@@ -14,6 +15,13 @@ interface NavigatorProps {
 }
 
 export default class PanelNavigator extends React.Component<NavigatorProps, NavigatorState> {
+	icons = new Map([
+		[DataGroup.METADATA, <TagIcon />],
+		[DataGroup.COLORS, <ColorIcon />],
+		[DataGroup.IMAGES, <ImagesIcon />],
+		[DataGroup.DATA, <DataIcon />]
+	]);
+
 	constructor(props: NavigatorProps) {
 		super(props);
 
@@ -49,13 +57,14 @@ export default class PanelNavigator extends React.Component<NavigatorProps, Navi
 			);
 		});
 
-		const groups = Array.from(this.props.fields.entries(), ([group, details]) => {
+		const groups = Array.from<[DataGroup, FieldDetails[]], JSX.Element>(this.props.fields.entries(), ([group, details]) => {
 			return (
 				<PanelGroup
 					name={group}
 					key={group}
 					isActive={group === this.state.activePanel}
 					setActive={this.selectOpenPanel}
+					icon={this.icons.get(group)}
 				>
 					{details.map((data => {
 						const { kind, name, ...otherData } = data;
