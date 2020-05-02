0b0e420183adcb5251bcc1d65d294dbc4a659abc
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-03 01:59:50 +0200

    Added TextPanel events for keydown and blur

--- src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
@@ -5,6 +5,19 @@ import "./style.less";
 interface TextPanelProps extends PanelProps { }
 
 export default function TextPanel(props: TextPanelProps) {
+	const onKeyDownEventRef = React.useRef(({ key }: React.KeyboardEvent) => {
+		if (key === "Enter") {
+			inputRef.current && inputRef.current.blur();
+		}
+	});
+
+	const onBlurEventRef = React.useRef(() => {
+		// perform save
+		console.log("//// SAVING VALUE FOR", props.name);
+	});
+
+	const inputRef = React.useRef<HTMLInputElement>(null);
+
 	const showTitle = `${props.name.slice(0, 1).toUpperCase()}${props.name.slice(1)}`
 		.replace(/([a-z])([A-Z])/g, "$1 $2");
 
@@ -16,7 +29,13 @@ export default function TextPanel(props: TextPanelProps) {
 	return (
 		<div className="panel text" data-name={props.name}>
 			<label htmlFor={props.name}><h4>{showTitle}</h4>{required}</label>
-			<input id={props.name} placeholder={props.name} />
+			<input
+				id={props.name}
+				placeholder={props.name}
+				ref={inputRef}
+				onBlur={onBlurEventRef.current}
+				onKeyDown={onKeyDownEventRef.current}
+			/>
 		</div>
 	);
 }
