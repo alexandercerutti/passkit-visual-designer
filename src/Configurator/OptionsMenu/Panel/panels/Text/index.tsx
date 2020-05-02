5780c5b8d4ae493e1d97900824adbcfc2b4434d0
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-03 01:12:57 +0200

    Added required plate near text fields

--- src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
@@ -8,9 +8,14 @@ export default function TextPanel(props: TextPanelProps) {
 	const showTitle = `${props.name.slice(0, 1).toUpperCase()}${props.name.slice(1)}`
 		.replace(/([a-z])([A-Z])/g, "$1 $2");
 
+	const required = (
+		props.data.required &&
+		<span className="required" />
+	) || null;
+
 	return (
 		<div className="panel text" data-name={props.name}>
-			<label htmlFor={props.name}><h4>{showTitle}</h4></label>
+			<label htmlFor={props.name}><h4>{showTitle}</h4>{required}</label>
 			<input id={props.name} placeholder={props.name} />
 		</div>
 	);
