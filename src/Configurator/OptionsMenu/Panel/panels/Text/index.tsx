ec6dcb19e83e282076245e80f99af4cf06d49ea1
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-02 17:23:34 +0200

    Added first structure and style for text panels

--- src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
@@ -2,10 +2,16 @@ import * as React from "react";
 import { PanelProps } from "../..";
 import "./style.less";
 
-interface TextPanelProps extends Omit<PanelProps, "kind"> { }
+interface TextPanelProps extends PanelProps { }
 
 export default function TextPanel(props: TextPanelProps) {
+	const showTitle = `${props.name.slice(0, 1).toUpperCase()}${props.name.slice(1)}`
+		.replace(/([a-z])([A-Z])/g, "$1 $2");
+
 	return (
-		<div></div>
+		<div className="panel text" data-name={props.name}>
+			<label htmlFor={props.name}><h4>{showTitle}</h4></label>
+			<input id={props.name} placeholder={props.name} />
+		</div>
 	);
 }
