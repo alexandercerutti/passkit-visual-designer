c21402ff9f83aea86de1dc3568c7863378fd6458
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-06 23:27:11 +0200

    Removed inputRef react ref from Text Panel for event currentTarget

--- src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Text/index.tsx
@@ -5,18 +5,14 @@ import "./style.less";
 interface TextPanelProps extends PanelProps { }
 
 export default function TextPanel(props: TextPanelProps) {
-	const onKeyDownEventRef = React.useRef(({ key }: React.KeyboardEvent) => {
-		if (key === "Enter") {
-			inputRef.current && inputRef.current.blur();
-		}
+	const onKeyDownEventRef = React.useRef(({ key, currentTarget }: React.KeyboardEvent<HTMLInputElement>) => {
+		key === "Enter" && currentTarget.blur();
 	});
 
 	const onBlurEventRef = React.useRef(() => {
 		// perform save
 		console.log("//// SAVING VALUE FOR", props.name);
 	});
-
-	const inputRef = React.useRef<HTMLInputElement>(null);
 
 	const showTitle = `${props.name.slice(0, 1).toUpperCase()}${props.name.slice(1)}`
 		.replace(/([a-z])([A-Z])/g, "$1 $2");
@@ -32,7 +28,6 @@ export default function TextPanel(props: TextPanelProps) {
 			<input
 				id={props.name}
 				placeholder={props.name}
-				ref={inputRef}
 				onBlur={onBlurEventRef.current}
 				onKeyDown={onKeyDownEventRef.current}
 			/>
