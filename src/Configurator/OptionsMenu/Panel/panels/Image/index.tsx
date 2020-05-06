009b6ef0ce4e6eb05068af8c0d345265de9991ea
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-06 23:14:26 +0200

    Initial creation of Image panel

--- src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
@@ -2,10 +2,21 @@ import * as React from "react";
 import { PanelProps } from "../..";
 import "./style.less";
 
-interface ImagePanelProps extends Omit<PanelProps, "kind"> { }
+interface ImagePanelProps extends PanelProps {
+	// value: ArrayBuffer;
+}
 
 export default function ImagePanel(props: ImagePanelProps) {
+	const [picture, setPicture] = React.useState<Blob>(null);
+
+	const showTitle = props.name.replace(/([a-z])([A-Z])/g, "$1 $2");
+
 	return (
-		<div></div>
+		<div className="panel image" data-name={props.name}>
+			<h4>{showTitle}</h4>
+			<div className="dd-area">
+				<span>Icon</span>
+			</div>
+		</div>
 	);
 }
