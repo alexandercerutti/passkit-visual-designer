354d0da60d059f729dd7306c3f2df665c3aa7480
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-10 01:34:44 +0200

    Changed Image big component to be easier and divided

--- src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
@@ -5,28 +5,72 @@ import "./style.less";
 import UploadIcon from "./uploadIcon";
 
 interface ImagePanelProps extends PanelProps {
-	// value: ArrayBuffer;
-}
-
-const enum FileState {
-	NONE,
-	LOADED,
-	LOADING
-}
-
-interface PanelState {
-	state: FileState;
-	file: File;
+	fileURL?: string;
 }
 
 export default function ImagePanel(props: ImagePanelProps) {
-	const [picture, setPicture] = React.useState<File>(null);
-	const [dragging, setDragging] = React.useState(false);
-	const areaRef = React.useRef<HTMLDivElement>(null);
+	const showTitle = props.name.replace(/([a-z])([A-Z])/g, "$1 $2");
 
+	const onFileUploadHandlerRef = React.useRef((file: File) => {
+		// Send the url outside
+	});
+
+	return (
+		<div className="panel image" data-name={props.name}>
+			<h4>{showTitle}</h4>
+			{props.fileURL
+				? <PictureShowdown name={props.name} pictureURL={props.fileURL} />
+				: <UploadArea onFileUpload={onFileUploadHandlerRef.current} />
+			}
+		</div>
+	);
+}
+
+interface PictureShowdownProps {
+	name: string;
+	pictureURL: string;
+}
+
+function PictureShowdown(props: PictureShowdownProps): JSX.Element {
+	return (
+		<img src={props.pictureURL} alt={props.name} />
+	);
+}
+
+interface UploadAreaProps {
+	onFileUpload: (file: File) => void;
+}
+
+function UploadArea(props: UploadAreaProps): JSX.Element {
 	const inputFileHandlerRef = React.useRef((files: FileList) => {
 		console.log("Received some files to handle.", files);
+		const firstFile = files[0];
+		props.onFileUpload(firstFile);
 	});
+
+	const onChangeHandlerRef = React.useRef((event: React.ChangeEvent<HTMLInputElement>) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		inputFileHandlerRef.current(event.target.files);
+	});
+
+	return (
+		<>
+			<input type="file" id="fileUpload" hidden onChange={onChangeHandlerRef.current} />
+			<label htmlFor="fileUpload">
+				<DragAndDropArea onFilesUploaded={inputFileHandlerRef.current} />
+			</label>
+		</>
+	);
+}
+
+interface DnDProps {
+	onFilesUploaded: (files: FileList) => void;
+}
+
+function DragAndDropArea(props: DnDProps): JSX.Element {
+	const [dragging, setDragging] = React.useState(false);
 
 	const onDragOverHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault();
@@ -46,35 +90,19 @@ export default function ImagePanel(props: ImagePanelProps) {
 		event.stopPropagation();
 
 		setDragging(false);
-		inputFileHandlerRef.current(event.dataTransfer.files);
+		props.onFilesUploaded(event.dataTransfer.files);
 	});
-
-	const onChangeHandlerRef = React.useRef((event: React.ChangeEvent<HTMLInputElement>) => {
-		event.preventDefault();
-		event.stopPropagation();
-
-		inputFileHandlerRef.current(event.target.files);
-	});
-
-	const showTitle = props.name.replace(/([a-z])([A-Z])/g, "$1 $2");
 
 	return (
-		<div className="panel image" data-name={props.name}>
-			<h4>{showTitle}</h4>
-			<input type="file" id="fileUpload" hidden value={picture && picture.name} onChange={onChangeHandlerRef.current} />
-			<label htmlFor="fileUpload">
-				<div
-					className="dd-area"
-					ref={areaRef}
-					onDragOver={onDragOverHandlerRef.current}
-					onDragLeave={onDragLeaveHandlerRef.current}
-					onDrop={onDropHandlerRef.current}
-					style={{ borderStyle: dragging ? "solid" : "dashed" }}
-				>
-					<UploadIcon width="50px" height="50px" fill="#cacaca" />
-					<span>Click or drag to upload</span>
-				</div>
-			</label>
+		<div
+			className="dd-area"
+			onDragOver={onDragOverHandlerRef.current}
+			onDragLeave={onDragLeaveHandlerRef.current}
+			onDrop={onDropHandlerRef.current}
+			style={{ borderStyle: dragging ? "solid" : "dashed" }}
+		>
+			<UploadIcon width="50px" height="50px" fill="#cacaca" />
+			<span>Click or drag to upload</span>
 		</div>
 	);
 }
