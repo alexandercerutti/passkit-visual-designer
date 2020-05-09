caf7edcef9cbc9f4272ab7abe700042da56c5a4d
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-10 00:02:46 +0200

    Added ImagePanel style, content and basic upload by select or drag and drop

--- src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
@@ -2,21 +2,79 @@ import * as React from "react";
 import { PanelProps } from "../..";
 import "./style.less";
 
+import UploadIcon from "./uploadIcon";
+
 interface ImagePanelProps extends PanelProps {
 	// value: ArrayBuffer;
 }
 
+const enum FileState {
+	NONE,
+	LOADED,
+	LOADING
+}
+
+interface PanelState {
+	state: FileState;
+	file: File;
+}
+
 export default function ImagePanel(props: ImagePanelProps) {
-	const [picture, setPicture] = React.useState<Blob>(null);
+	const [picture, setPicture] = React.useState<File>(null);
+	const [dragging, setDragging] = React.useState(false);
+	const areaRef = React.useRef<HTMLDivElement>(null);
+
+	const inputFileHandlerRef = React.useRef((files: FileList) => {
+		console.log("Received some files to handle.", files);
+	});
+
+	const onDragOverHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		if (!dragging) {
+			setDragging(true);
+		}
+	});
+
+	const onDragLeaveHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) =>
+		setDragging(false)
+	);
+
+	const onDropHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		setDragging(false);
+		inputFileHandlerRef.current(event.dataTransfer.files);
+	});
+
+	const onChangeHandlerRef = React.useRef((event: React.ChangeEvent<HTMLInputElement>) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		inputFileHandlerRef.current(event.target.files);
+	});
 
 	const showTitle = props.name.replace(/([a-z])([A-Z])/g, "$1 $2");
 
 	return (
 		<div className="panel image" data-name={props.name}>
 			<h4>{showTitle}</h4>
-			<div className="dd-area">
-				<span>Icon</span>
-			</div>
+			<input type="file" id="fileUpload" hidden value={picture && picture.name} onChange={onChangeHandlerRef.current} />
+			<label htmlFor="fileUpload">
+				<div
+					className="dd-area"
+					ref={areaRef}
+					onDragOver={onDragOverHandlerRef.current}
+					onDragLeave={onDragLeaveHandlerRef.current}
+					onDrop={onDropHandlerRef.current}
+					style={{ borderStyle: dragging ? "solid" : "dashed" }}
+				>
+					<UploadIcon width="50px" height="50px" fill="#cacaca" />
+					<span>Click or drag to upload</span>
+				</div>
+			</label>
 		</div>
 	);
 }
---
+++ src/Configurator/OptionsMenu/Panel/panels/Image/uploadIcon.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+// Upload by Alice Design from the Noun Project (edited)
+// https://thenounproject.com/term/upload/2969619
+export default function UploadIcon(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg viewBox="0 0 97.76 62.79" {...props}>
+			<path d="M79.54 62.79a18.31 18.31 0 0018.22-18.58c0-10.39-8.13-18.87-18.22-18.87a18.41 18.41 0 00-3.4.32A16.56 16.56 0 0059.79 9.22a16.13 16.13 0 00-5.56 1A22.1 22.1 0 0035.71 0C23.36 0 13.38 10.28 13.38 23a26.77 26.77 0 00.21 2.86A18.68 18.68 0 000 44a18.46 18.46 0 0018.22 18.77z" />
+			<path d="M44.1,62.8l0-17.5h-6.2c-1.5,0-2.4-1.8-1.5-3l11-14.9c0.8-1,2.3-1,3,0l11,14.9c0.9,1.2,0,3-1.5,3h-6.2v17.5 H44.1z" fill="#2d2d2d" />
+		</svg>
+	);
+}
