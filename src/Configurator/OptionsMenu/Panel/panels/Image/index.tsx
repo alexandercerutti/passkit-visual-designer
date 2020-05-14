a6e95eac99626397505f01487a97cf2f07669b79
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-14 19:33:29 +0200

    Moved PictureShowdown component to its own file

--- src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Image/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { PanelProps } from "../..";
 import UploadArea from "./UploadArea";
+import PictureShowdown from "./PictureShowdown";
 import "./style.less";
-import { EditIcon, DeleteIcon } from "./icons";
 
 interface ImagePanelProps extends PanelProps {
 	value?: string;
@@ -28,43 +28,6 @@ export default function ImagePanel(props: ImagePanelProps) {
 				/>
 				: <UploadArea onFileUpload={onChosenFileChangedHandlerRef.current} />
 			}
-		</div>
-	);
-}
-
-interface PictureShowdownProps {
-	name: string;
-	pictureData: Blob;
-	onDelete: () => void;
-}
-
-function PictureShowdown(props: PictureShowdownProps): JSX.Element {
-	const [pictureURL, setPictureURL] = React.useState(null);
-
-	React.useEffect(() => {
-		if (pictureURL !== props.pictureData) {
-			URL.revokeObjectURL(pictureURL);
-			setPictureURL(URL.createObjectURL(props.pictureData));
-		}
-
-		if (!props.pictureData) {
-			props.onDelete();
-		}
-
-		return () => URL.revokeObjectURL(pictureURL);
-	}, [props.pictureData]);
-
-	return (
-		<div className="picture">
-			<img src={pictureURL} alt={props.name} />
-			<div className="opts">
-				<div>
-					<EditIcon fill="#e6e6e6" />
-				</div>
-				<div onClick={props.onDelete}>
-					<DeleteIcon fill="#e6e6e6" />
-				</div>
-			</div>
 		</div>
 	);
 }
