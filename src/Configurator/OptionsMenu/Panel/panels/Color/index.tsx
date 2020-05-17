895de63f351c5648448df5d714d5f0e163b00731
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-18 00:33:55 +0200

    Added implementation and styles for ColorPanel

--- src/Configurator/OptionsMenu/Panel/panels/Color/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Color/index.tsx
@@ -1,11 +1,36 @@
 import * as React from "react";
+import { TwitterPicker, RGBColor, ColorResult } from "react-color";
 import { PanelProps } from "../..";
 import "./style.less";
 
-interface ColorPanelProps extends Omit<PanelProps, "kind"> { }
+interface ColorPanelProps extends PanelProps {
+	value?: string;
+}
 
 export default function ColorPanel(props: ColorPanelProps) {
+	const [color, setColor] = React.useState<string>(props.value || "rgb(0,0,0)");
+	const showTitle = props.name.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+	const onColorChange = React.useRef((color: ColorResult) => {
+		const rgbColor = rgbaToRGBString(color.rgb);
+		setColor(rgbColor);
+		props.onValueChange(props.name, rgbColor);
+	});
+
 	return (
-		<div></div>
+		<>
+			<h4>{showTitle}</h4>
+			<TwitterPicker
+				triangle="hide"
+				// PR #403 on react-color: https://github.com/casesandberg/react-color/pull/403 TO FIX
+				className="color-selector"
+				color={color}
+				onChangeComplete={onColorChange.current}
+			/>
+		</>
 	);
+}
+
+function rgbaToRGBString({ r, g, b }: RGBColor) {
+	return `rgb(${r},${g},${b})`;
 }
