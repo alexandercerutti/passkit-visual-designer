7f0d0b93e548c8ada7e095b71f005381a2dc599f
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-05-20 00:11:06 +0200

    Added support to react-color history changing when a new color is selected

--- src/Configurator/OptionsMenu/Panel/panels/Color/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Color/index.tsx
@@ -11,8 +11,23 @@ export default function ColorPanel(props: ColorPanelProps) {
 	const [color, setColor] = React.useState<string>(props.value || "rgb(0,0,0)");
 	const showTitle = props.name.replace(/([a-z])([A-Z])/g, "$1 $2");
 
-	const onColorChange = React.useRef((color: ColorResult) => {
-		const rgbColor = rgbaToRGBString(color.rgb);
+	// Default react-color hashes
+	const { current: colorHistory } = React.useRef<string[]>([
+		"#ff6900", "#fcb900", "#7bdcb5", "#00d084", "#8ed1fc",
+		"#0693e3", "#abb8c3", "#eb144c", "#f78da7", "#9900ef"
+	]);
+
+	const onColorChange = React.useRef(({ rgb, hex }: ColorResult) => {
+		const colorInRecentlyUsedIndex = colorHistory.indexOf(hex);
+
+		if (colorInRecentlyUsedIndex === -1) {
+			colorHistory.unshift(hex);
+			colorHistory.pop(); // to keep them 10
+		} else {
+			colorHistory.unshift(...colorHistory.splice(colorInRecentlyUsedIndex, 1));
+		}
+
+		const rgbColor = rgbaToRGBString(rgb);
 		setColor(rgbColor);
 		props.onValueChange(props.name, rgbColor);
 	});
@@ -25,6 +40,7 @@ export default function ColorPanel(props: ColorPanelProps) {
 				// PR #403 on react-color: https://github.com/casesandberg/react-color/pull/403 TO FIX
 				className="color-selector"
 				color={color}
+				colors={colorHistory}
 				onChangeComplete={onColorChange.current}
 			/>
 		</>
