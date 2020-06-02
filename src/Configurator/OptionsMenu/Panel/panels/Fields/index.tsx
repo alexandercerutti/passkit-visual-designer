74949f7225362e1f6d257eceeedea32eb021f318
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-06-02 11:36:07 +0200

    Added first basic version of structure for Fields panel with clicking and new pages adding

--- src/Configurator/OptionsMenu/Panel/panels/Fields/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Fields/index.tsx
@@ -1,10 +1,37 @@
 import * as React from "react";
 import { PanelProps } from "../..";
+import { FieldProps } from "../../../../../passes/Areas/components/Field";
+import { FieldsArrow } from "./icons";
+import FieldTitle from "../FieldTitle";
 import "./style.less";
 
-interface FieldPanelProps extends Omit<PanelProps, "kind"> { }
+interface FieldPanelProps extends PanelProps {
+	value?: FieldProps[];
+}
 
 export default function FieldPanel(props: FieldPanelProps) {
+	const { current: pageCreationClickHandler } = React.useRef(() => {
+		props.requestPageCreation(
+			<FieldInternalPanel
+
+			/>
+		);
+	});
+
+	return (
+		<>
+			<div className="cta-edit" onClick={pageCreationClickHandler}>
+				<div className="col-left">
+					<FieldTitle name={props.name} />
+					<em>{`${props.value?.length ?? 0} fields for this area`}</em>
+				</div>
+				<FieldsArrow />
+			</div>
+		</>
+	);
+}
+
+function FieldInternalPanel() {
 	return (
 		<div></div>
 	);
