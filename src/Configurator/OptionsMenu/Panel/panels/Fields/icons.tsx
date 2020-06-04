c20d5a84aa99fbabe1736473bad41a4c55dfb780
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-06-05 00:04:21 +0200

    Added more fields below indicator

--- src/Configurator/OptionsMenu/Panel/panels/Fields/icons.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Fields/icons.tsx
@@ -18,3 +18,14 @@ export const FieldsAddIcon = (props: React.SVGProps<SVGSVGElement>) => (
 		<path d="M84.42 68.5H68.5v16.22a4.8 4.8 0 01-4.5 4.8 4.65 4.65 0 01-4.5-4.8V68.5H43.58a4.5 4.5 0 01-4.5-4.5 4.35 4.35 0 014.2-4.5H59.5V43.28a4.8 4.8 0 014.5-4.8 4.65 4.65 0 014.5 4.8V59.5h15.92a4.5 4.5 0 014.5 4.5 4.35 4.35 0 01-4.2 4.5z" />
 	</svg>
 );
+
+// dots by Andrejs Kirma from the Noun Project (edited)
+// https://thenounproject.com/term/dots/2915594
+
+export const MoreFieldsBelowIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg viewBox="0 0 25 4" {...props}>
+		<circle cx={9} cy={2} r={2} />
+		<circle cx={16} cy={2} r={2} />
+		<circle cx={2} cy={2} r={2} />
+	</svg>
+);
--- src/Configurator/OptionsMenu/Panel/panels/Fields/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Fields/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { PanelProps } from "../..";
 import { FieldProps } from "../../../../../passes/Areas/components/Field";
-import { FieldsArrowIcon, FieldsAddIcon } from "./icons";
+import { FieldsArrowIcon, FieldsAddIcon, MoreFieldsBelowIcon } from "./icons";
 import FieldTitle from "../FieldTitle";
 import "./style.less";
 
@@ -40,11 +40,36 @@ interface FieldInternalPanel extends Omit<FieldPanelProps, "requestPageClosing" 
 
 function FieldInternalPanel(props: FieldInternalPanel) {
 	const [fields, setFields] = React.useState(props.value || []);
+	const [isThereMoreAfterTheSkyline, setMoreAvailability] = React.useState(false);
+	const listRef = React.useRef<HTMLDivElement>();
+
 	const name = `${props.name.slice(0, 1).toUpperCase()}${props.name.slice(1)}`;
 
 	const { current: newFieldClickHandler } = React.useRef((fields: FieldProps[]) => {
 		setFields(fields);
 	});
+
+	React.useEffect(() => {
+		const { current: fieldList } = listRef;
+
+		fieldList.addEventListener("scroll", () => {
+			const didReachEndOfScroll = (
+				fieldList.scrollHeight - fieldList.scrollTop === fieldList.clientHeight
+			);
+			// We want to hide "more" icon if we reached end of the scroll
+			setMoreAvailability(!didReachEndOfScroll);
+		});
+	}, []);
+
+	React.useEffect(() => {
+		const { current: fieldList } = listRef;
+		const { children } = listRef.current.parentNode;
+		const [header] = Array.from(children) as HTMLDivElement[];
+
+		if (fields.length) {
+			setMoreAvailability((header.offsetHeight + fieldList.scrollHeight) > window.innerHeight);
+		}
+	}, [fields]);
 
 	return (
 		<div className="fields-page">
@@ -56,8 +81,11 @@ function FieldInternalPanel(props: FieldInternalPanel) {
 				<FieldTitle name={name} />
 				<FieldsAddIcon onClick={() => newFieldClickHandler([...fields, {} as FieldProps])} />
 			</header>
-			<div className="fields">
+			<div className="fields" ref={listRef}>
 				<FieldsDrawer {...props} value={fields} />
+			</div>
+			<div className="more-items-indicator" style={{ opacity: Number(isThereMoreAfterTheSkyline) }}>
+				<MoreFieldsBelowIcon />
 			</div>
 		</div>
 	);
