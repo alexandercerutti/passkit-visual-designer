e43d0610c97f29726f1ffcabbe7867aad67067e6
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-06-14 18:17:55 +0200

    Renamed listRef to drawerRef and improved references in useEffect

--- src/Configurator/OptionsMenu/Panel/panels/Fields/FieldsDrawer/index.tsx
+++ src/Configurator/OptionsMenu/Panel/panels/Fields/FieldsDrawer/index.tsx
@@ -10,20 +10,19 @@ interface FieldsDrawerProps {
 
 export default function FieldsDrawer(props: FieldsDrawerProps) {
 	const [isThereMoreAfterTheSkyline, setMoreAvailability] = React.useState(false);
-	const listRef = React.useRef<HTMLDivElement>();
+	const drawerRef = React.useRef<HTMLDivElement>();
 
 	const onListScrollHandler = React.useRef(() => {
 		const didReachEndOfScroll = (
-			listRef.current.scrollHeight - listRef.current.scrollTop === listRef.current.clientHeight
+			drawerRef.current.scrollHeight - drawerRef.current.scrollTop === drawerRef.current.clientHeight
 		);
 		// We want to hide "more" icon if we reached end of the scroll
 		setMoreAvailability(!didReachEndOfScroll);
 	});
 
 	React.useEffect(() => {
-		const { current: fieldList } = listRef;
-		const { children } = listRef.current.parentNode;
-		const [header] = Array.from(children) as HTMLDivElement[];
+		const { current: fieldList } = drawerRef;
+		const [header] = Array.from(fieldList.parentNode.children) as HTMLDivElement[];
 
 		if (props.fieldsData.length) {
 			setMoreAvailability((header.offsetHeight + fieldList.scrollHeight) > window.innerHeight);
@@ -40,7 +39,11 @@ export default function FieldsDrawer(props: FieldsDrawerProps) {
 
 	return (
 		<>
-			<div className="fields" ref={listRef} onScroll={onListScrollHandler.current}>
+			<div
+				className="fields"
+				ref={drawerRef}
+				onScroll={onListScrollHandler.current}
+			>
 				{panels}
 			</div>
 			<div className="more-items-indicator" style={{ opacity: Number(isThereMoreAfterTheSkyline) }}>
