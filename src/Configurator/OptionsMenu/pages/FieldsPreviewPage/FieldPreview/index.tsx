6fd83f7eeacacffe9352d3f8753d6871b927ba9a
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-06-30 23:27:15 +0200

    Added FieldPreview edit mode

--- src/Configurator/OptionsMenu/pages/FieldsPreviewPage/FieldPreview/index.tsx
+++ src/Configurator/OptionsMenu/pages/FieldsPreviewPage/FieldPreview/index.tsx
@@ -1,23 +1,56 @@
 import * as React from "react";
 import "./style.less";
 import { FieldProperties } from "../../../../../passes/Areas/components/Field/fieldCommons";
+import { createClassName } from "../../../../../passes/utils";
 
 interface Props {
 	previewData: FieldProperties;
 	isFieldHidden?: boolean;
+	keyEditable?: boolean;
 	onClick?(): void;
+	onFieldKeyChange?(newValue: string): void;
 }
 
 export default function FieldPreview(props: Props) {
 	const isPresetFieldKey = props.previewData.fieldKey.includes("::pkvd-new");
+	const [fieldKey, setFieldKey] = React.useState(!isPresetFieldKey ? props.previewData.fieldKey : "");
+
+	React.useEffect(() => {
+		if (fieldKey && fieldKey !== props.previewData.fieldKey) {
+			props.onFieldKeyChange(fieldKey);
+		}
+	}, [fieldKey]);
+
+	const FPClassName = createClassName(["field-preview"], {
+		"hidden": props.isFieldHidden,
+		"editable": props.keyEditable
+	});
+
+	const previewKeyClassName = createClassName(["preview-field-key"], {
+		"none": isPresetFieldKey
+	});
+
+	const fieldKeyRow = props.keyEditable
+		? (
+			<input
+				type="text"
+				onChange={(evt) => setFieldKey(evt.target.value)}
+				value={fieldKey}
+				placeholder="field key"
+			/>
+		) : (
+			<span>
+				{isPresetFieldKey ? "not setted" : props.previewData?.fieldKey}
+			</span>
+		);
 
 	return (
 		<div
-			className={`field-preview${props.isFieldHidden && " hidden" || ""}`}
+			className={FPClassName}
 			onClick={props?.onClick}
 		>
-			<div className={`preview-field-key${isPresetFieldKey && " none" || ""}`}>
-				<span>{isPresetFieldKey ? "not setted" : props.previewData?.fieldKey}</span>
+			<div className={previewKeyClassName}>
+				{fieldKeyRow}
 			</div>
 			<div className="preview-main-box">
 				<span className="label">{props.previewData.label || "label"}</span>
