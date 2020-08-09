7d09f1132fc4557508d0f6cfe9c0104b5f022ac1
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-08-09 17:15:15 +0200

    Changed DEFAULT_LABEL_COLOR and empty background color

--- src/Pass/constants.ts
+++ src/Pass/constants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_BACKGROUND_COLOR = "#e6e6e6";
 export const DEFAULT_FOREGROUND_COLOR = "#000";
-export const DEFAULT_LABEL_COLOR = "#000";
+export const DEFAULT_LABEL_COLOR = "#cecece";
 
 export enum PKDateStyle {
 	None = "PKDateStyleNone", /** ??? */
