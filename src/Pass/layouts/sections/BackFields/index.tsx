d284928f6a0be94a4c85a73901daa28b9cfb7b3d
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-08-14 12:40:37 +0200

    Added FieldsRow elements for BackFields to filter objects that have value and label

--- src/Pass/layouts/sections/BackFields/index.tsx
+++ src/Pass/layouts/sections/BackFields/index.tsx
@@ -1,22 +1,23 @@
 import * as React from "react";
 import "./style.less";
-import { FieldProperties } from "../../components/Field/fieldCommons";
 import FieldsRow from "../FieldRow";
 import InteractionContext from "../../../InteractionContext";
+import { PassFieldKeys } from "../../../constants";
 
 interface Props {
-	data: FieldProperties[];
+	data: PassFieldKeys[];
 }
 
 export default function Backfields(props: Props) {
 	const context = React.useContext(InteractionContext);
+	const validFieldsAmount = props.data && props.data.filter(({ value, label }) => value || label) || [];
 
 	return (
 		<div className="back-fields">
 			<FieldsRow
 				id="backFields"
-				maximumElementsAmount={-1}
-				elements={props.data}
+				maximumElementsAmount={0}
+				elements={validFieldsAmount}
 				register={context.registerField}
 				onClick={context.onFieldSelect}
 				className={!validFieldsAmount.length && "expand" || ""}
