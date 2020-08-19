5e88414df6363aba5e76553af81a5fe86701bf41
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-08-19 23:29:21 +0200

    First version of ExportModal window

---
+++ src/Configurator/ExportModal/index.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import "./style.less";
+
+/**
+ * This modal must receive some data to generate, for each "partner"
+ * the model importing example.
+ *
+ * To describe every partner, I need a way to describe code chunks for
+ * each field, epecially mocked ones.
+ *
+ */
+
+export default function ExportModal() {
+	const [activePartnerTab, setActivePartnerTab] = React.useState(0);
+
+	const partnersMock = ["Partner 1", "Partner 2", "Partner 3"];
+
+	const partnerTabs = partnersMock.map((partner, index) => (
+		<div
+			className={`tab${index === activePartnerTab && " active" || ""}`}
+			onClick={() => setActivePartnerTab(index)}
+			key={`tab${index}`}
+		>
+			{partner}
+		</div>
+	));
+
+	return (
+		<div className="export-modal">
+			<h2>Great!</h2>
+			<p>Your model is now being exported. Here below you can see some open source libraries examples to generate programmatically passes with your mock data compiled. Enjoy!</p>
+			<div className="tabs">
+				{partnerTabs}
+			</div>
+			<div className="partner-example"></div>
+		</div>
+	);
+}
