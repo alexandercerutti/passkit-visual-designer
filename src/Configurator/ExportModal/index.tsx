1ea74959d390e3d70919ebf462ad212ce5a9688a
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-09-04 23:39:56 +0200

    Defined Partner structure and made partners arriving to ExportModel from parent

--- src/Configurator/ExportModal/index.tsx
+++ src/Configurator/ExportModal/index.tsx
@@ -5,6 +5,8 @@ import "prismjs/plugins/line-numbers/prism-line-numbers";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/themes/prism.css";
 import "prismjs/themes/prism-tomorrow.css";
+import { createClassName } from "../../utils";
+import { replaceAllBlock } from "./transformers";
 
 /**
  * This modal must receive some data to generate, for each "partner"
@@ -15,20 +17,43 @@ import "prismjs/themes/prism-tomorrow.css";
  *
  */
 
-export default function ExportModal() {
+interface Partner {
+	name: string;
+	lang: string; // @TODO to evaluate the insertion of language
+	template: string;
+}
+
+interface Props {
+	partners: Partner[];
+}
+
+export default function ExportModal(props: Props) {
 	const [activePartnerTab, setActivePartnerTab] = React.useState(0);
 
-	const partnersMock = ["Partner 1", "Partner 2", "Partner 3"];
+	const partnerTabs = props.partners.map((partner, index) => {
+		const className = createClassName(["tab"], {
+			"active": index === activePartnerTab
+		});
 
-	const partnerTabs = partnersMock.map((partner, index) => (
-		<div
-			className={`tab${index === activePartnerTab && " active" || ""}`}
-			onClick={() => setActivePartnerTab(index)}
-			key={`tab${index}`}
-		>
-			{partner}
-		</div>
-	));
+		return (
+			<div
+				className={className}
+				onClick={() => setActivePartnerTab(index)}
+				key={`tab${index}`}
+			>
+				{partner.name}
+			</div>
+		);
+	});
+
+	const { template, lang } = props.partners[activePartnerTab];
+
+	const partnerFilledContent = replaceAllBlock(template, {
+		description: "capocchium",
+		serialNumber: undefined
+	});
+
+	const codeLanguage = `language-${lang}`;
 
 	return (
 		<div id="export-modal">
@@ -38,7 +63,14 @@ export default function ExportModal() {
 				<div className="tabs">
 					{partnerTabs}
 				</div>
-				<div className="partner-example"></div>
+				<div className="partner-example">
+					<pre className="line-numbers">
+						<code
+							className={codeLanguage}
+							dangerouslySetInnerHTML={{ __html: Prism.highlight(partnerFilledContent, Prism.languages[lang], lang) }}
+						/>
+					</pre>
+				</div>
 			</div>
 			<div id="close-overlay"></div>
 		</div>
