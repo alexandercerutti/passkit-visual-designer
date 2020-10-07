1b14340d282c001a5fb0d9dbbc263077d55f0ab4
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-10-07 17:34:56 +0200

    Improved generation of clippers elements and added fallback element if no clipper source is available (playstation games tribute)

--- src/Configurator/MediaModal/CollectionsView/index.tsx
+++ src/Configurator/MediaModal/CollectionsView/index.tsx
@@ -12,22 +12,48 @@ interface Props {
 }
 
 export default function CollectionsView(props: Props) {
-	const collectionsElements = props.collections?.map((coll) => (
-		<div className="collection" key={coll.name} onClick={props.selectCollection}>
-			<div className="preview">
-				<div className="clipper">
-					<img src={coll.srcset[0] || ""} />
+	const collectionsElements = props.collections?.map((coll, index) => {
+		let previewContent: React.ReactFragment;
+
+		if (coll.srcset.length) {
+			const trimmedSet = coll.srcset.slice(0, 3);
+			const fallbackElement = trimmedSet[trimmedSet.length - 1];
+			const previewSet = Array<string>(3).fill(undefined);
+			previewSet.splice(0, Math.min(3, coll.srcset.length), ...trimmedSet);
+
+			const clippers = previewSet.map((url, index) => {
+				const src = url || fallbackElement;
+				return (
+					<div className="clipper" key={`${coll.name}-clipper${index}`}>
+						<img src={src} />
+					</div>
+				);
+			});
+
+			previewContent = (
+				<>
+					{clippers}
+				</>
+			);
+		} else {
+			previewContent = (
+				<>
+					<div className="empty-slot">
+						<span className="fade-blink" title="Add pictures to this collection to see something">Empty Slot</span>
+					</div>
+				</>
+			);
+		}
+
+		return (
+			<div className="collection" key={`${coll.name}-collection${index}`} onClick={props.selectCollection}>
+				<div className="preview">
+					{previewContent}
 				</div>
-				<div className="clipper">
-					<img src={coll.srcset[1] || coll.srcset[coll.srcset.length - 1]} />
-				</div>
-				<div className="clipper">
-					<img src={coll.srcset[2] || coll.srcset[coll.srcset.length - 1]} />
-				</div>
+				<span>{coll.srcset.length && coll.name || ""}</span>
 			</div>
-			<span>{coll.name}</span>
-		</div>
-	));
+		)
+	});
 
 	const amountOfRows = Math.ceil((props.collections.length + 1) / 3);
 
