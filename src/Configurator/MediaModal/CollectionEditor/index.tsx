4344fcad02e302c8a947337cef3c180b335bf11d
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-11-03 00:21:42 +0100

    Added support for adding a new resolution via both click and d&d

--- src/Configurator/MediaModal/CollectionEditor/index.tsx
+++ src/Configurator/MediaModal/CollectionEditor/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { IdentifiedResolutions, MediaCollection } from "../../../store/state";
+import { v1 as uuid } from "uuid";
+import { IdentifiedResolutions, MediaCollection, ResolutionTuple } from "../../../store/state";
 import AddElementButton from "../AddElementButton";
 import "./style.less";
 
@@ -12,26 +13,39 @@ interface Props {
 export default function CollectionEditor(props: Props) {
 	const [draggingOver, setDraggingOver] = React.useState(false);
 
-	const onDragEnterHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) => {
+	const onDragEnterHandlerRef = React.useCallback((event: React.DragEvent<HTMLDivElement>) => {
 		preventPropagation(event);
 
 		if (!draggingOver) {
 			setDraggingOver(true);
 		}
-	});
+	}, [draggingOver]);
 
 	const onDragLeaveHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) => {
 		preventPropagation(event);
-		setDraggingOver(false)
-	});
-
-	const onDropHandlerRef = React.useRef((event: React.DragEvent<HTMLDivElement>) => {
-		preventPropagation(event);
-		console.log("Dragging done", event.dataTransfer.files);
-
 		setDraggingOver(false);
-		// props.onFilesUploaded(event.dataTransfer.files);
 	});
+
+	const onDropHandlerRef = React.useCallback(async (event: React.DragEvent<HTMLDivElement>) => {
+		preventPropagation(event);
+		setDraggingOver(false);
+
+		const newResolutions = await getResolutionsFromFileList(event.dataTransfer.files);
+
+		props.onResolutionChange(props.collectionID, {
+			...props.collection.resolutions,
+			...newResolutions,
+		}, 0b0010);
+	}, [props.collection]);
+
+	const onChangeHandlerRef = React.useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newResolutions = await getResolutionsFromFileList(event.target.files);
+
+		props.onResolutionChange(props.collectionID, {
+			...props.collection.resolutions,
+			...newResolutions,
+		}, 0b0010);
+	}, [props.collection]);
 
 	const onKeyDownEventRef = React.useRef(({ key, currentTarget }: React.KeyboardEvent<HTMLInputElement>) => {
 		if (key === "Enter") {
@@ -83,23 +97,26 @@ export default function CollectionEditor(props: Props) {
 			<div
 				id="grid"
 				className={`collection-editor ${draggingOver && "dragOver" || ""}`}
-				onDragEnter={onDragEnterHandlerRef.current}
+				onDragEnter={onDragEnterHandlerRef}
 				// To avoid for children pictures to be dragged and trigger the onDragEnter
 				onDragStart={preventPropagation}
 			>
 				{collectionItems}
 				<div className="item">
-					<AddElementButton
-						caption="Add picture"
-						onClick={() => void 0}
-					/>
+					<input type="file" id="file-upload" hidden onChange={onChangeHandlerRef} />
+					<label htmlFor="file-upload">
+						<AddElementButton
+							caption="Add picture"
+							onClick={() => void 0}
+						/>
+					</label>
 				</div>
 			</div>
 			<div
 				id="drop-area"
 				onDragOver={preventPropagation}
 				onDragLeave={onDragLeaveHandlerRef.current}
-				onDrop={onDropHandlerRef.current}
+				onDrop={onDropHandlerRef}
 			>
 				<span>Drop to add to collection</span>
 			</div>
@@ -110,4 +127,22 @@ export default function CollectionEditor(props: Props) {
 function preventPropagation(e: React.SyntheticEvent) {
 	e.preventDefault();
 	e.stopPropagation();
+}
+
+async function getResolutionsFromFileList(files: FileList) {
+	const buffers = await Promise.all<ArrayBuffer>(
+		Array.prototype.map.call(files, (fileObj: File) =>
+			fileObj.arrayBuffer()
+		)
+	);
+
+	return buffers.reduce((acc, buffer) => {
+		return {
+			...acc,
+			[uuid()]: {
+				name: "",
+				content: [buffer, null] as ResolutionTuple
+			}
+		}
+	}, {} as IdentifiedResolutions);
 }
