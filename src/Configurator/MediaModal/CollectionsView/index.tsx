8f315eb3a194803e8b88558c04a9fe3bb02bb40f
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-11-09 22:20:55 +0100

    Fixed CollectionView filter for activeCollectionID

--- src/Configurator/MediaModal/CollectionsView/index.tsx
+++ src/Configurator/MediaModal/CollectionsView/index.tsx
@@ -32,64 +32,66 @@ export default function CollectionsView(props: Props) {
 		props.performCollectionsOperation(CollectionEditCreate);
 	}, [props.collections]);
 
-	const collectionsElements = Object.entries(props.collections).map(([collID, collection], index) => {
-		let previewContent: React.ReactFragment;
-		const resolutionsIDs = Object.keys(collection.resolutions);
+	const collectionsElements = Object.entries(props.collections)
+		.filter(([collID]) => collID !== "activeCollectionID")
+		.map(([collID, collection], index) => {
+			let previewContent: React.ReactFragment;
+			const resolutionsIDs = Object.keys(collection.resolutions);
 
-		if (resolutionsIDs.length) {
-			const trimmedSet = resolutionsIDs.slice(0, 3);
-			const fallbackElementURL = collection.resolutions[trimmedSet[trimmedSet.length - 1]].content[1];
+			if (resolutionsIDs.length) {
+				const trimmedSet = resolutionsIDs.slice(0, 3);
+				const fallbackElementURL = collection.resolutions[trimmedSet[trimmedSet.length - 1]].content[1];
 
-			/**
-			 * Creating a sized array of resolutions.
-			 * splicing from 0 to resolutions amount to not
-			 * remove undefined unused spaces.
-			 */
+				/**
+				 * Creating a sized array of resolutions.
+				 * splicing from 0 to resolutions amount to not
+				 * remove undefined unused spaces.
+				 */
 
-			const previewSet = Array<string>(3).fill(undefined);
-			previewSet.splice(
-				0,
-				Math.min(3, resolutionsIDs.length),
-				...trimmedSet.map(key => collection.resolutions[key].content[1])
-			);
-
-			const clippers = previewSet.map((url, index) => {
-				const src = url || fallbackElementURL;
-				return (
-					<div className="clipper" key={`${collection.name}-clipper${index}`}>
-						<img src={src} />
-					</div>
+				const previewSet = Array<string>(3).fill(undefined);
+				previewSet.splice(
+					0,
+					Math.min(3, resolutionsIDs.length),
+					...trimmedSet.map(key => collection.resolutions[key].content[1])
 				);
-			});
 
-			previewContent = (
-				<>
-					{clippers}
-				</>
-			);
-		} else {
-			previewContent = (
-				<>
-					<div className="empty-slot">
-						<span className="fade-blink" title="Add pictures to this collection to see something">Empty Slot</span>
-					</div>
-				</>
-			);
-		}
+				const clippers = previewSet.map((url, index) => {
+					const src = url || fallbackElementURL;
+					return (
+						<div className="clipper" key={`${collection.name}-clipper${index}`}>
+							<img src={src} />
+						</div>
+					);
+				});
 
-		return (
-			<div className="collection" key={`${collection.name}-collection${index}`}>
-				<div className="preview" onClick={() => props.onCollectionUse(collID)}>
-					{previewContent}
-					<div className={collectionsClassName}>
-						<EditIcon id="edit-coll" onClick={(e) => void e.stopPropagation() || collectionEditClickHandler(collID)} />
-						<DeleteIcon id="delete-coll" onClick={(e) => void e.stopPropagation() || collectionDeleteClickHandler(collID)} />
+				previewContent = (
+					<>
+						{clippers}
+					</>
+				);
+			} else {
+				previewContent = (
+					<>
+						<div className="empty-slot">
+							<span className="fade-blink" title="Add pictures to this collection to see something">Empty Slot</span>
+						</div>
+					</>
+				);
+			}
+
+			return (
+				<div className="collection" key={`${collection.name}-collection${index}`}>
+					<div className="preview" onClick={() => props.onCollectionUse(collID)}>
+						{previewContent}
+						<div className={collectionsClassName}>
+							<EditIcon id="edit-coll" onClick={(e) => void e.stopPropagation() || collectionEditClickHandler(collID)} />
+							<DeleteIcon id="delete-coll" onClick={(e) => void e.stopPropagation() || collectionDeleteClickHandler(collID)} />
+						</div>
 					</div>
+					<span>{resolutionsIDs.length && collection.name || "no-name"}</span>
 				</div>
-				<span>{resolutionsIDs.length && collection.name || "no-name"}</span>
-			</div>
-		);
-	});
+			);
+		});
 
 	return (
 		<>
