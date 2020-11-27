76f2f44b4e69a38ebd00722bda6510a178af5bef
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-11-27 18:02:24 +0100

    Added mapping of languages as json (omg, kill me pls)

---
+++ src/Configurator/LanguageModal/languages.ts
@@ -0,0 +1,129 @@
+/**
+ * @see https://stackoverflow.com/questions/3040677/locale-codes-for-iphone-lproj-folders
+ */
+
+export const languages = {
+	"Europe": {
+		"en": "English",
+		"gd": "Scottish Gaelic",
+		"cy": "Welsh", // Wales
+		"gv": "Manx", // Ireland / Celtic
+		"fr": "French", // France, Belgique, Luxembourg, Suisse
+		"br": "Breton", // Brittany
+		"de": "German", // Deutschland, Liechtenstein, Österreich, Schweiz
+		"it": "Italian", // Italia
+		"mt": "Maltese", // Malta
+		"nl": "Dutch", // Nederland
+		"es": "Spanish", // España
+		"ca": "Catalan", // España
+		"gl": "Galician", // Galicia (España)
+		"da": "Danish", // Danmark
+		"fo": "Faroese", // Danmark
+		"pt": "Portuguese", // Portugal
+		"no": "Norwegian Bokmål", // Norge
+		"nn": "Norwegian Nynorsk", // Norge
+		"sv": "Swedish", // Sverige
+		"fi": "Finnish", // Suomi
+		"se": "Northern Sami", // Norge, Sverige and Suomi
+		"lt": "Lithuanian", // Lietuva
+		"et": "Estonian", // Eesti
+		"lv": "Latvian", // Latvija
+		"el": "Greek", // Ελλάδα
+		"is": "Icelandic", // ísland
+		"pl": "Polish", // Polska
+		"hr": "Croatian", // Hrvatska
+		"hu": "Hungarian", // Magyarorszag
+		"ga": "Irish", // Ireland
+		"ru": "Russian", // Россия
+		"tt": "Tatar", // Tatarstan, Россия, Siberian
+		"sq": "Albanian", // Albania
+		"ro": "Romanian", // România
+		"cs": "Czech", // Česko
+		"sk": "Slovak", // Slovensko
+		"sl": "Slovenian", // Slovenia
+		"mk": "Macedonian", // North Macedonia
+		"bg": "Bulgarian", // България
+		"uk": "Ukrainian", // Ukrayina
+		"be": "Belarusian", // Беларусь
+		"eu": "Basque", // Basques
+		"mo": "Moldavian", // Moldova
+		"sr": "Serbian", // српски
+		"tr": "Turkish", // Türkiye
+		// Montenegrin // Montenegro ???
+	},
+	"Asia Pacific": {
+		"zh-Hant": "Chinese", // 中国大陆, 澳門, 台灣
+		"zh": "Chinese / Mandarin", // 中国大陆, 澳門, 台灣
+		"ug": "Uyghur", // China
+		"mn": "Mongolian", // Mongolia
+		"ja": "Japanese", // 日本
+		"ko": "Korean", // 대한민국
+		"id": "Indonesian", // Indonesia / bahasa Indonesia
+		"ms": "Malay", // bahasa Malaysia, Singapore
+		"th": "Thai", // ไทย
+		"vi": "Vietnamese", // Vietnam / Cộng hòa Xã hội chủ nghĩa Việt Nam
+		"km": "Khmer", // Cambodia
+		"lo": "Lao", // Lao
+		"my": "Burmese", // Myanmar
+		"tl": "Tagalog", // Philippines
+		"jv": "Javanese", // Indonesia
+		"su": "Sundanese", // Indonesia
+		"to": "Tongan", // Tonga
+	},
+	"Africa, Middle East and India": {
+		"hy": "Armenian", // Armenia (Հայաստան)
+		"ar": "Arabic", // Bahrain Arabic (البحرين), مصر (Egypt), الأردن (Jordan), الكويت (Kuwait), اللهجة المغربية (Morocco), عُمان (Oman), قطر (Qatar), المملكة العربية السعودية (Saudi Arabia), Tunisie, الإمارات العربية المتحدة (United Arab Emirates)
+		// "sg": "Sango", // République Centrafricaine (unsupported?)
+		"hi": "Hindi", // भारत (India)
+		"ur": "Urdu", // India, Pakistan
+		"sd": "Sindhi", // India
+		"mr": "Marathi", // India
+		"bn": "Bengali", // India
+		"pa": "Punjabi", // Punjab (Culture)
+		"or": "Oriya", // Odisha
+		"ml": "Malayalam", // Kerala
+		"kn": "Kannada", // Karnataka
+		"ta": "Tamil", // Tamil Nadu
+		"te": "Telugu", // Andhra Pradesh, Telangana, Yanam district of Puducherry
+		"si": "Sinhala", // Sri Lanka
+		"as": "Assamese", // Assam
+		"gu": "Gujarati", // Gujarat
+		"he": "Hebrew", // ישראל (Israel)
+		"yi": "Yiddish", // Israel
+		"sw": "Swahili", // Keyna, Uganda
+		"mg": "Malagasy", //  Repoblikan'i Madagasikara (Madagascar)
+		"af": "Afrikaans", // South Africa
+		"fa": "Persian", // Iran
+		"uz": "Uzbek", // Uzbekistan
+		"kk": "Kazakh", // Kazakhstan
+		"ka": "Georgian", // Georgia
+		"ky": "Kyrgyz", // Kyrgyzstan
+		"tg": "Tajik", // Tajikistan
+		"tk": "Turkmen", // Turkmenistan
+		"ps": "Pashto", // Iran
+		"ku": "Kurdish", // Iran, Kurdistan
+		"ks": "Kashmiri", // Jammu and Kashmir (ex India)
+		"bo": "Tibetan", // Tibet
+		"ne": "Nepali", // Nepal
+		"am": "Amharic", // Amharas (North Ethiopia)
+		"ti": "Tigrinya", // North Ethiopia, Eritrea
+		"om": "Oromo", // Ethiopia
+		"so": "Somali", // Federal Republic of Somalia
+		"rw": "Kinyarwanda", // Rwanda, Eastern Democratic Republic of the Congo, Uganda
+		"rn": "Rundi", // Burundi, Democratic Republic of the Congo, Uganda
+		"dz": "Dzongkha", // Bhutan
+		"az": "Azerbaijani", // Azerbaijan
+		"sa": "Sanskrit",
+	},
+	"Latin America and the Caribbean": {
+		"la": "Latin", // Spanish
+		"qu": "Quechua",
+		"gn": "Guarani", // Paraguay, Bolivia, Argentina, Brazil
+		"ay": "Aymara", // Bolivia, Peru and Chile
+	},
+	"Others": {
+		"eo": "Esperanto", // All the world
+		"iu": "Inuktitut", // Canada
+		"kl": "Kalaallisut", // Greennland
+	},
+};
