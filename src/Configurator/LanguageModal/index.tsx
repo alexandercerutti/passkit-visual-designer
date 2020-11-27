c5bda688804ac99636db32363412ab121c34e0fd
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-11-27 18:29:53 +0100

    Added first version of LanguageModal content parsing

--- src/Configurator/LanguageModal/index.tsx
+++ src/Configurator/LanguageModal/index.tsx
@@ -1,18 +1,39 @@
 import * as React from "react";
 import "./style.less";
 import Modal from "../ModalBase";
+import { languages } from "./languages";
 
 interface Props {
 	closeModal(): void;
+	selectLanguage(languageCode: string): void;
 }
 
 export default function LanguageModal(props: Props) {
+	const languagesRegions = Object.entries(languages).map(([region, languages]) => {
+		const languagesList = Object.entries(languages).map(([ISO639alpha1, language]) => (
+			<div onClick={() => props.selectLanguage(ISO639alpha1)} title={`Language code: ${ISO639alpha1}`}>
+				{language}
+			</div>
+		));
+
+		return (
+			<>
+				<h2>{region}</h2>
+				<div className="languages-grid">
+					{languagesList}
+				</div>
+			</>
+		);
+	});
+
 	return (
 		<Modal
 			closeModal={props.closeModal}
 			contentUniqueID="language"
 		>
-
+			<div className="languages-list">
+				{languagesRegions}
+			</div>
 		</Modal>
 	);
 }
