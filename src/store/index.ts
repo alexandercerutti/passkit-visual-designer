adfa95967779a22a72e00b0dde88178114ca99fa
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-12-02 00:46:54 +0100

    Fixed store components recursive dependency and crash

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,66 +1,6 @@
+export * from "./store"; /** Important here because others below depend on this */
 export * as Media from "./media";
 export * as Pass from "./pass";
 export * as Options from "./projectOptions";
 export { default as reducers } from "./reducers";
 export * as middlewares from "./middlewares";
-
-import { PassMixedProps, MediaProps } from "../Pass";
-
-export const initialState: State = {
-	pass: {},
-	media: {
-		// "default": {
-		/* 			backgroundImage: {
-						enabled: true,
-						activeCollectionID: "",
-						collections: {}
-					} */
-		// }
-	},
-	projectOptions: {
-		activeMediaLanguage: "default"
-	},
-};
-
-export interface State {
-	pass: Partial<PassMixedProps>;
-	media: LocalizedMediaGroup;
-	projectOptions: {
-		title?: string,
-		activeMediaLanguage: string;
-	};
-}
-
-export type LocalizedMediaGroup = {
-	[languageOrDefault: string]: MediaSet;
-};
-
-export type MediaSet = {
-	[K in keyof MediaProps]: CollectionSet;
-}
-
-export type CollectionSet = {
-	activeCollectionID: string;
-	enabled: boolean;
-	collections: {
-		[collectionID: string]: MediaCollection;
-	};
-}
-
-export interface MediaCollection {
-	name: string;
-	resolutions: IdentifiedResolutions;
-};
-
-export type IdentifiedResolutions = {
-	[resolutionID: string]: {
-		name: string;
-		content: ResolutionTuple;
-	}
-};
-
-/**
- * string is optional because we generate the
- * url in redux middleware
- */
-export type ResolutionTuple = [ArrayBuffer, string?];
---
+++ src/store/store.ts
@@ -0,0 +1,60 @@
+import type { PassMixedProps, MediaProps } from "../Pass";
+
+export const initialState: State = {
+	pass: {},
+	media: {
+		// "default": {
+		/* 			backgroundImage: {
+						enabled: true,
+						activeCollectionID: "",
+						collections: {}
+					} */
+		// }
+	},
+	projectOptions: {
+		activeMediaLanguage: "default"
+	},
+};
+
+export interface State {
+	pass: Partial<PassMixedProps>;
+	media: LocalizedMediaGroup;
+	projectOptions: {
+		title?: string,
+		activeMediaLanguage: string;
+	};
+}
+
+export type LocalizedMediaGroup = {
+	[languageOrDefault: string]: MediaSet;
+};
+
+export type MediaSet = {
+	[K in keyof MediaProps]: CollectionSet;
+}
+
+export type CollectionSet = {
+	activeCollectionID: string;
+	enabled: boolean;
+	collections: {
+		[collectionID: string]: MediaCollection;
+	};
+}
+
+export interface MediaCollection {
+	name: string;
+	resolutions: IdentifiedResolutions;
+};
+
+export type IdentifiedResolutions = {
+	[resolutionID: string]: {
+		name: string;
+		content: ResolutionTuple;
+	}
+};
+
+/**
+ * string is optional because we generate the
+ * url in redux middleware
+ */
+export type ResolutionTuple = [ArrayBuffer, string?];
