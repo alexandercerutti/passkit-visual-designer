034d40b62d3c740e207349e421a663e4ed448f07
Alexander Cerutti <cerutti.alexander@gmail.com>
2020-12-17 13:10:37 +0100

    Added translations update notification to outside and onChange methods to inputs

--- src/Configurator/TranslationsModal/index.tsx
+++ src/Configurator/TranslationsModal/index.tsx
@@ -1,19 +1,38 @@
 import * as React from "react";
+import { TranslationsSet } from "../../store";
 import Modal, { ModalProps } from "../ModalBase";
 import "./style.less";
 
+const TranslationChangePlaceholder = 0b001;
+const TranslationChangeValue = 0b010;
+export type TranslationChangeOps = typeof TranslationChangePlaceholder | typeof TranslationChangeValue;
+
 interface Props extends Omit<ModalProps, "contentUniqueID"> {
 	currentLanguage: string;
-	availableTranslations: [string, string][];
+	availableTranslations: TranslationsSet;
 	requestForLanguageChange(): void;
-	onTranslationsChange(): void;
+	editTranslation(id: string, placeholder: string, value: string): void;
+	removeTranslation(id: string): void;
+	addTranslation(): void;
 }
 
 export default function TranslationsModal(props: Props) {
-	const translations = props.availableTranslations.map(([placeholder, value]) => (
+	const onInputValueChange = React.useCallback((id: string, changeOP: TranslationChangeOps, changedValue: string) => {
+		const newPayload: TranslationsSet["translations"][0] = [];
+
+		if (changeOP & TranslationChangeValue) {
+			newPayload.push(props.availableTranslations.translations[id][0], changedValue);
+		} else if (changeOP & TranslationChangePlaceholder) {
+			newPayload.push(changedValue, props.availableTranslations.translations[id][1]);
+		}
+
+		props.editTranslation(id, ...newPayload);
+	}, [props.availableTranslations]);
+
+	const translations = Object.entries(props.availableTranslations.translations).map(([id, [placeholder, value]]) => (
 		<React.Fragment key={`trl-${placeholder}`}>
-			<input type="text" value={placeholder || ""} />
-			<input type="text" value={value || ""} />
+			<input type="text" value={placeholder || ""} onChange={(e) => onInputValueChange(id, TranslationChangePlaceholder, e.currentTarget.value)} />
+			<input type="text" value={value || ""} onChange={(e) => onInputValueChange(id, TranslationChangeValue, e.currentTarget.value)} />
 		</React.Fragment>
 	));
 
