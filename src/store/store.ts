14fd22b8e4068c879f5e9626f6564695da69862f
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-03-15 21:45:13 +0100

    Added testing data to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,6 +8,80 @@ declare const __DEV__: boolean;
 const __DEV_DEFAULT_PASS_PROPS = __DEV__ ? {
 	transitType: PKTransitType.Boat,
 	kind: PassKind.BOARDING_PASS,
+	/** FEW TESTING DATA **/
+	/**
+
+	backgroundColor: "rgb(255,99,22)",
+	labelColor: "rgb(0,0,0)",
+	foregroundColor: "rgb(255,255,255)",
+	headerFields: [
+		{
+			key: "num",
+			label: "volo",
+			value: "EJU996",
+			textAlignment: PKTextAlignment.Center,
+		},
+		{
+			key: "date",
+			label: "Data",
+			value: "21 set",
+			textAlignment: PKTextAlignment.Center,
+		}
+	],
+	primaryFields: [
+		{
+			key: "from",
+			label: "Venezia Marco Polo",
+			value: "VCE",
+			textAlignment: PKTextAlignment.Left,
+		},
+		{
+			key: "to",
+			label: "Napoli",
+			value: "NAP",
+			textAlignment: PKTextAlignment.Right,
+		},
+	],
+	auxiliaryFields: [
+		{
+			key: "user",
+			label: "Passeggero",
+			value: "SIG. ALEXANDER PATRICK CERUTTI",
+			textAlignment: PKTextAlignment.Left,
+		}, {
+			key: "seat",
+			label: "Posto",
+			value: "1C*",
+			textAlignment: PKTextAlignment.Center,
+		}
+	],
+	secondaryFields: [
+		{
+			key: "Imbarco",
+			label: "Imbarco chiuso",
+			value: "18:40",
+			textAlignment: PKTextAlignment.Center,
+		},
+		{
+			key: "takeoff",
+			label: "Partenza",
+			value: "19:10",
+			textAlignment: PKTextAlignment.Center,
+		},
+		{
+			key: "SpeedyBoarding",
+			label: "SB",
+			value: "Si",
+			textAlignment: PKTextAlignment.Center,
+		},
+		{
+			key: "boarding",
+			label: "Imbarco",
+			value: "Anteriore",
+			textAlignment: PKTextAlignment.Center,
+		}
+	]
+	*/
 } : null;
 
 export const initialState: State = {
